19086a16e5e58f41ed8e6a5ce211c34a8281dc87
jmlb0003 <jmlb0003@red.ujaen.es>
2015-06-07 12:52:22 +0200

    Lista de preguntas
    Se añade una lista de varias preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,6 +1,38 @@
 var models = require("../models/models.js");
 
-//GET /quizes/question
+//GET /quizes
+exports.index = function(req, res) {
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index.ejs', {quizes: quizes});
+	})
+};
+
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+	models.Quiz.find(req.params.quizId)
+		.then(function(quiz) {
+			res.render('quizes/show', { quiz: quiz});
+		})
+};
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+	models.Quiz.find(req.params.quizId)
+		.then(function(quiz) {
+			if (req.query.respuesta === quiz.respuesta) {
+				res.render( 'quizes/answer', 
+	    			{ quiz: quiz, respuesta: "Correcto" }
+	    		);
+			}else{
+				res.render( 'quizes/answer', 
+	    			{ quiz: quiz, respuesta: "Incorrecto" }
+	    		);			
+			}
+		})
+};
+
+/*//GET /quizes/question
 exports.question = function(req, res) {
 	//res.render('quizes/question', {pregunta: 'Capital de Italia'});
 	models.Quiz.findAll().then(function(quiz) {
@@ -19,3 +51,5 @@ exports.answer = function(req,res) {
 		}
 	});	
 };
+
+*/
--- models/quiz.js
+++ models/quiz.js
@@ -4,6 +4,6 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define(
     'Quiz',
     { pregunta: DataTypes.STRING,
-      respuesta: DataTypes.STRING,
+      respuesta: DataTypes.STRING
     });
 }
