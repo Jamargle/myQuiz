ee09f89f7d31500b1300f820c0ec0b7652c2e21c
jmlb0003 <jmlb0003@red.ujaen.es>
2015-06-09 11:42:40 +0200

    Bug de create y modificación de _form
    El then de validate daba null al crear una pregunta.
    El formulario genérico se ha modificado para que cuando gane el foco se
    borre el contenido

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -69,18 +69,14 @@ exports.new = function(req, res) {
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
-  quiz.validate()
-  .then(function(err){
-      if (err) {
-        res.render('quizes/new', {quiz: quiz, errors: err.errors});
-      } else {
-        quiz // save: guarda en DB campos pregunta y respuesta de quiz
-        .save({fields: ["pregunta", "respuesta"]})
-        .then( function(){ res.redirect('/quizes')}) 
-      }
-    }).catch(function(error) {
-      next(error);
-    });
+  var err = quiz.validate();
+  if (err){
+    res.render('quizes/new', {quiz: quiz, errors: err.errors});
+  }else{
+    quiz // save: guarda en DB campos pregunta y respuesta de quiz
+    .save({fields: ["pregunta", "respuesta"]})
+    .then( function(){ res.redirect('/quizes')}) 
+  }
 };
 
 // GET /quizes/:id/edit
