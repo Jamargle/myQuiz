105f23380f345b5c272d81431fde9bcc3e1767a8
jmlb0003 <jmlb0003@red.ujaen.es>
2015-06-11 11:30:53 +0200

    Añadir sesiones y autenticación

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,6 +11,11 @@ router.get('/', function(req, res) {
 
 //Ruta para cargar con el id de pregunta
 router.param('quizId', quizController.load);
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -21,7 +27,7 @@ router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
-
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
 
